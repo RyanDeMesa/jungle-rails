165eded570864dfe46688388fc5ebf62d367073d
Ryan De Mesa <ryandemesa09@gmail.com>
2023-01-31 01:43:55 +0000

    Add admin categories page & new category functionality

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ApplicationRecord
 
   has_many :products
-
+  
+  validates :name, presence: true, length: { minimum: 3}
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,11 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
-
+  
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
