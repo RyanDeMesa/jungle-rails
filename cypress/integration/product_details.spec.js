6eaac9b136a5c762a6098f2653104a9ffdf6f314
Ryan De Mesa <ryandemesa09@gmail.com>
2023-02-01 03:56:56 +0000

    Add tests for product details

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -1,13 +1,8 @@
-describe('Jungle Home Page', () => {
-
+describe('Jungle Product Details Page', () => {
+  
   beforeEach(() => {
     cy.visit("/");
   });
-
-  it("should visit root", () => {
-    cy.visit("/");
-  });
-
 
   it("There is products on the page", () => {
     cy.get(".products article").should("be.visible");
@@ -17,5 +12,10 @@ describe('Jungle Home Page', () => {
     cy.get(".products article").should("have.length", 2);
   });
 
-
+  // test that users can navigate from the home page to the product details page
+  it("Shows product/show", () => {
+    cy.get(".products article").first().click();
+    cy.get('.btn').contains("Add");
+  });
+  
 });
